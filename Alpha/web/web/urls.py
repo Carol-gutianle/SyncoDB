fea8c14bbe27acb3395270c5bebea43c22e257c9
lxthnu <9822557+lxthnu@user.noreply.gitee.com>
2022-06-29 11:15:52 +0800

    web

--- Alpha/web/web/urls.py
+++ Alpha/web/web/urls.py
@@ -1,64 +1,37 @@
-# Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.decorators import csrf
-import json
+"""matlabweb URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+# from django.conf.global_settings import MEDIA_ROOT
+from django.urls import path, re_path, include
+from django.views.static import serve
+from . import views
+from pathlib import Path
 import os
-import numpy as np
-from django.views.decorators.csrf import csrf_exempt
-from pylab import *
-# mpl.rcParams['font.sans-serif'] = ['SimHei']
-# titles=["图一反应条件：448K 1.0MPa","图二反应条件：453K 1.0MPa","图三反应条件：458K 1.0MPa","图四反应条件：463K 1.0MPa","图五反应条件：443K 1.0MPa 酯浓度0.002mol/m3","图六反应条件：443K 1.0MPa 酯浓度0.001mol/m3","图七反应条件：443K 1.0MPa 酯浓度0.004mol/m3"]
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+BASE_DIR = Path(__file__).resolve().parent.parent
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+urlpatterns = {
+    path('admin/', admin.site.urls),
+    # path('api/return_datas/', views.show_datas),
+    path('api/connect/', views.page1_check),
+    path('api/getsql/', views.page2_getsql),
+    re_path(r'media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
 
-password="123456"
-username="admin"
-ip="127.0.0.1"
-port="9000"
+}
 
-
-@csrf_exempt
-def page1_check(request):
-    request.encoding = 'utf-8'
-    data = {}
-
-    '''取出用户名，密码'''
-    # 以可读方式打开文件
-    fp = open("data.json", 'r')
-    json_data = json.load(fp)
-    global username,password,ip,port
-    username = json_data["username"]
-    password = json_data["pass"]
-    fp.close()
-    name_=request.GET.get('username','')
-    pass_=request.GET.get('pwd','')
-    ip_=request.GET.get('ip','')
-    port_ = request.GET.get('port','')
-
-    if (username==name_)==False:
-        rescode=201  #用户名错误
-    elif (password==pass_)==False:
-        rescode = 202  # 密码错误
-    elif (ip==ip_)==False:
-        rescode = 203  # IP错误
-    elif (port==port_)==False:
-        rescode = 203  # port错误
-    else :
-        rescode=200
-
-    data['res']=rescode
-    sample = json.dumps(data)  # json.dumps()把一个Python对象编，码转换成Json字符串。
-    return HttpResponse(sample, content_type="application/json")  # 返回给前端
-
-@csrf_exempt
-def page2_getsql(request):
-    request.encoding = 'utf-8'
-    sql=request.GET.get('request','')
-    data = {}
-    res=''
-    #调用c函数，将c函数返回的结果保存在res当中
-    data['res'] = res
-    HttpResponse(sample, content_type="application/json")  # 返回给前端
-
-
-
+# #设置静态文件路径
+# urlpatterns += staticfiles_urlpatterns()
 

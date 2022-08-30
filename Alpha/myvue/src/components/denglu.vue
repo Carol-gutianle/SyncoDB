5881cf40d55461ed5e22800d8d861a414a10dd8a
lxthnu <58248490+lxthnu@users.noreply.github.com>
2022-08-30 09:51:22 +0800

    周一修改

--- Alpha/myvue/src/components/denglu.vue
+++ Alpha/myvue/src/components/denglu.vue
@@ -33,7 +33,7 @@
         </el-form>
 
         <div style="text-align: center">
-          <el-button type="primary" icon="el-icon-edit" @click="save('form')">登录</el-button>
+          <el-button type="primary" icon="el-icon-edit" @click="save()">登录</el-button>
         </div>
 
       </el-card>
@@ -80,29 +80,29 @@ export default {
   },
 
   methods:{
-    save(form) {
-      this.$refs[form].validate((valid) => {
-        if (valid) {
+    gocaozuo(){
+      this.$router.push({
+        path:'../caozuo',
+      })
+    },
+    save() {
+      request.post("api/checkConn/", this.form).then(res => {
 
-          request.post("/api/checkConn/", this.form).then(res => {
-
-            console.log(res.data); //打印出来
-            if ((res.data.status == 200)) {
-              this.$message({
-                type: 'success',
-                message: '连接成功！'
-              })
-              //跳转到操作界面
-              this.gocaozuo();
-            } else {
-              this.$message({
-                type: "error",
-                message: "用户名/密码错误！"
-              })
-            }
+        console.log(res.data); //打印出来
+        if ((res.data.status == 200)) {
+          this.$message({
+            type: 'success',
+            message: '连接成功！'
+          })
+          //跳转到操作界面
+          this.gocaozuo();
+        } else {
+          this.$message({
+            type: "error",
+            message: "用户名/密码错误！"
           })
         }
-      });
+      })
     },
   },
 

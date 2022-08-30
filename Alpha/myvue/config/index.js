cc39788668e090d3f75e665841ab325620642f0c
lxthnu <58248490+lxthnu@users.noreply.github.com>
2022-08-30 09:49:43 +0800

    周一修改

--- Alpha/myvue/config/index.js
+++ Alpha/myvue/config/index.js
@@ -12,11 +12,11 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       "/api": {
-        target: "http://127.0.0.1:8000", // 要访问的接口域名
+        target: "http://127.0.0.1:8000/", // 要访问的接口域名
         //ws: true, // 是否启用websockets
         changeOrigin: true, //开启代理：在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
         pathRewrite: {
-          "^/api": "" //这里理解成用'/api'代替target里面的地址,比如我要调用'http://40.00.100.100:3002/user/add'，直接写'/api/user/add'即可
+          "/api": "" //这里理解成用'/api'代替target里面的地址,比如我要调用'http://40.00.100.100:3002/user/add'，直接写'/api/user/add'即可
         }
     }},
 
